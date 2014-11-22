bee489c771523f8abbabe193c13f4f9888c9d902
Sebastian Willenborg <comawill@wlnbrg.de>
2014-11-22 22:54:59 +0100

    Created initial test distribution files

---
+++ test_install.sh
@@ -0,0 +1,5 @@
+installto="$(kpsewhich --var-value TEXMFLOCAL)/"
+sudo cp -r dist/fonts dist/tex $installto
+sudo updmap-sys --nomkmap --nohash --enable Map=YanoneKaffeesatzZeroHack.map
+sudo updmap-sys
+sudo mktexlsr
