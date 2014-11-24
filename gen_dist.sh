6b73430fa470ba7f2756e445d666f4d3ee9ac202
Sebastian Willenborg <comawill@wlnbrg.de>
2014-11-24 21:52:04 +0100

    gen_dist.sh missed the font .sty

--- gen_dist.sh
+++ gen_dist.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 cd src/YanoneKaffeesatzZeroHack
 make clean
 make all
@@ -10,7 +11,8 @@ cd ../../dist
 cp -r ../src/YanoneKaffeesatzZeroHack/fonts/ ./
 mkdir -p tex/latex/stratum0/img
 cp ../src/YanoneKaffeesatzZeroHack/*.fd tex/latex/stratum0
-cp ../src/beamer/*.sty tex/latex/stratum0
+cp ../src/YanoneKaffeesatzZeroHack/*.sty tex/latex/stratum0
+cp -r ../src/YanoneKaffeesatzZeroHack/fonts/ ./
 cp ../src/beamer/img/*.pdf tex/latex/stratum0/img
 zip -r stratum0.zip fonts tex -x "*/\.*" -x "\.*"
 cd ..
--- test_install.sh
+++ test_install.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 installto="$(kpsewhich --var-value TEXMFLOCAL)/"
 sudo cp -r dist/fonts dist/tex $installto
 sudo updmap-sys --nomkmap --nohash --enable Map=YanoneKaffeesatzZeroHack.map
